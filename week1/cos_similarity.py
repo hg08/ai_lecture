bf484ffaca761c565d806928b2b3e25b68056378
Huang Gang <huangjiadong08@yeah.net>
2018-06-26 23:38:14 +0800

    add cos_similarity.py, etc

---
+++ week1/cos_similarity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Calculate cos_similarity
+"""
+# 导入模块
+import numpy as np
+
+# 实现表示文档A,B,C的矢量a,b,c
+   ### 学生代码 ###
+
+# 计算文档A,B之间的相似度（其实是计算两个矢量夹角之余弦值）
+   ### 学生代码 ###
+
+# 输出相似度
+   ### 学生代码 ###
+
